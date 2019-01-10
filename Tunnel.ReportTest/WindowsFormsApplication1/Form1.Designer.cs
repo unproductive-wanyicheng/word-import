6de2c3e12c7c394b04ddf54c93f7f6282a372ff7
xiaowan2016 <17775728499@163.com>
2019-01-10 22:00:48 +0800

    test

--- Tunnel.ReportTest/WindowsFormsApplication1/Form1.Designer.cs
+++ Tunnel.ReportTest/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,36 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(190, 215);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(182, 145);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(883, 207);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(746, 306);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1076, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1121, 592);
+            this.ClientSize = new System.Drawing.Size(1208, 559);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -69,6 +81,7 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Tunnel.ReportTest/WindowsFormsApplication1/Form1.cs
+++ Tunnel.ReportTest/WindowsFormsApplication1/Form1.cs
@@ -79,32 +79,62 @@ namespace WindowsFormsApplication1
             //    MessageBox.Show("连接远程服务器失败!");
             //    Console.Write("finished");
             //}
+
             
 
             // 创建图表
             ChartArea area2 = new ChartArea("ChartArea2");
             chart1.ChartAreas.Add(area2);
-            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.White;
             chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Gray;
-            chart1.Legends[0].Position = new ElementPosition(50, 10, 10, 30);
-            chart1.Legends[0].BackColor = Color.Transparent;
-            chart1.Titles.Add("喷射混凝土厚度(cm)");
-            chart1.Titles[0].TextOrientation = TextOrientation.Horizontal;
-            chart1.Titles[0].Docking = Docking.Left;
-            //chart1.Titles[0]
-            chart1.Titles[0].Position = new ElementPosition(0, 10, 20, 10);
+            //chart1.ChartAreas[0].AxisX2.Enabled = AxisEnabled.True;
+            //chart1.ChartAreas[0].AxisX2.LineColor = Color.Red;
+            //chart1.ChartAreas[0].AxisX2.LineDashStyle = ChartDashStyle.Dash;
+            //chart1.ChartAreas[0].AxisX2.
+
+            chart1.Legends[0].Position = new ElementPosition(50, 5, 6, 5);
+            chart1.Legends[0].BackColor = Color.White;
+            chart1.Legends[0].LegendStyle = LegendStyle.Row;
+            chart1.Legends[0].Title = "E测线";
+            chart1.Legends[0].TitleFont = new Font("黑体", 9);
+            //LegendCellColumn legend1 = chart1.Legends[0].CellColumns[1];
+            //String s = chart1.Legends[0].LegendItemOrder;
+            //Console.WriteLine(s);
+            //legend1.Dispose();
+            // 不显示x轴
+            //chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.LineWidth = 3;
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = 60;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("黑体", 7);
+            //chart1.ChartAreas[0].AxisX.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Straight;
+            chart1.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+
+            chart1.ChartAreas[0].AxisY.LineWidth = 2;
+            chart1.ChartAreas[0].AxisY.Maximum = 70;
+            chart1.ChartAreas[0].AxisY.Minimum = 10;
+            chart1.ChartAreas[0].AxisY.Interval = 10;
+            //chart1.Titles.Add("喷射混凝土厚度(cm)");
+            //chart1.Titles[0].TextOrientation = TextOrientation.Horizontal;
+            //chart1.Titles[0].Docking = Docking.Left;
+            //chart1.Titles[0].Position = new ElementPosition(0, 10, 20, 10);
             chart1.Series.Remove(chart1.Series["Series1"]);
             chart1.Series.Add("张三");
             chart1.Series.Add("李四");
-            List<int> x = new List<int>() { 1, 2, 3, 4, 5 };
+            //chart1.Series[1].SmartLabelStyle.CalloutLineDashStyle = ChartDashStyle.Dash;
+            //chart1.Series[1].IsXValueIndexed = true;
 
-            List<int> y1 = new List<int>() { 10, 20, 30, 40, 50 };
-            List<int> y2 = new List<int>() { 15, 15, 15, 15, 15 };
+            List<String> x = new List<String>() { "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " " };
+
+            List<int> y1 = new List<int>() { 25, 35, 30, 25, 35, 25, 35, 30, 25, 35 };
+            List<int> y2 = new List<int>() { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 };
 
             chart1.Series["张三"].Points.DataBindXY(x, y1);
             chart1.Series["李四"].Points.DataBindXY(x, y2);
 
-            chart1.Series["张三"].ChartType = SeriesChartType.Line;
+            chart1.Series["张三"].ChartType = SeriesChartType.Spline;
             chart1.Series["张三"].Color = Color.FromArgb(0, 0, 128);
 
             chart1.Series["李四"].ChartType = SeriesChartType.Line;
@@ -114,9 +144,22 @@ namespace WindowsFormsApplication1
             {
                 Angle = -90
             };
-            a.Text = "11111111111111111";
+            a.Text = "喷射混凝土厚度(cm)";
             a.Visible = true;
+            a.Width = 30;
+            a.Height = 200;
+            a.Location = new Point(50, 100);
             this.Controls.Add(a);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 5);
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            pen.DashPattern = new float[] { 5f, 5f };
+            Graphics gh = this.CreateGraphics();
+            gh.DrawLine(pen, 10, 50, 150, 50);
         }
     }
 }
