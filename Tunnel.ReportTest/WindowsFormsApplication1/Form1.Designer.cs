61c2a9ed49c106a5b794ae2900e0e0e9c4bcda06
万宜成 <wanyichen>
2019-01-11 15:00:48 +0800

    test

--- Tunnel.ReportTest/WindowsFormsApplication1/Form1.Designer.cs
+++ Tunnel.ReportTest/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,60 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // chart1
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(182, 145);
-            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(746, 306);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1076, 102);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1208, 559);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(753, 297);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- Tunnel.ReportTest/WindowsFormsApplication1/Form1.cs
+++ Tunnel.ReportTest/WindowsFormsApplication1/Form1.cs
@@ -11,6 +11,7 @@ using Tunnel.Word.Test;
 using System.IO;
 using System.Net;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing.Drawing2D;
 
 namespace WindowsFormsApplication1
 {
@@ -80,77 +81,250 @@ namespace WindowsFormsApplication1
             //    Console.Write("finished");
             //}
 
-            
+            // 创建侧线图
+            Panel tableP = new Panel();
+            tableP.Location = new Point(20, 20);
+            tableP.Width = 1054;
+            tableP.Height = 660;
+            tableP.BackColor = Color.White;
+            tableP.Paint += new PaintEventHandler(panel1_Paint1);
 
-            // 创建图表
-            ChartArea area2 = new ChartArea("ChartArea2");
-            chart1.ChartAreas.Add(area2);
-            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
-            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.White;
-            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Gray;
-            //chart1.ChartAreas[0].AxisX2.Enabled = AxisEnabled.True;
-            //chart1.ChartAreas[0].AxisX2.LineColor = Color.Red;
-            //chart1.ChartAreas[0].AxisX2.LineDashStyle = ChartDashStyle.Dash;
-            //chart1.ChartAreas[0].AxisX2.
+            Panel cexianPanel = new Panel();
+            cexianPanel.Width = 1025;
+            cexianPanel.Height = 616;
+            cexianPanel.Location = new Point(25, 2);
+            cexianPanel.BackColor = Color.Transparent;
 
-            chart1.Legends[0].Position = new ElementPosition(50, 5, 6, 5);
-            chart1.Legends[0].BackColor = Color.White;
-            chart1.Legends[0].LegendStyle = LegendStyle.Row;
-            chart1.Legends[0].Title = "E测线";
-            chart1.Legends[0].TitleFont = new Font("黑体", 9);
-            //LegendCellColumn legend1 = chart1.Legends[0].CellColumns[1];
-            //String s = chart1.Legends[0].LegendItemOrder;
-            //Console.WriteLine(s);
-            //legend1.Dispose();
-            // 不显示x轴
-            //chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
-            chart1.ChartAreas[0].AxisX.Interval = 1;
-            chart1.ChartAreas[0].AxisX.LineWidth = 3;
-            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
-            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = 60;
-            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("黑体", 7);
-            //chart1.ChartAreas[0].AxisX.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Straight;
-            chart1.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
-
-            chart1.ChartAreas[0].AxisY.LineWidth = 2;
-            chart1.ChartAreas[0].AxisY.Maximum = 70;
-            chart1.ChartAreas[0].AxisY.Minimum = 10;
-            chart1.ChartAreas[0].AxisY.Interval = 10;
-            //chart1.Titles.Add("喷射混凝土厚度(cm)");
-            //chart1.Titles[0].TextOrientation = TextOrientation.Horizontal;
-            //chart1.Titles[0].Docking = Docking.Left;
-            //chart1.Titles[0].Position = new ElementPosition(0, 10, 20, 10);
-            chart1.Series.Remove(chart1.Series["Series1"]);
-            chart1.Series.Add("张三");
-            chart1.Series.Add("李四");
-            //chart1.Series[1].SmartLabelStyle.CalloutLineDashStyle = ChartDashStyle.Dash;
-            //chart1.Series[1].IsXValueIndexed = true;
-
-            List<String> x = new List<String>() { "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " " };
-
-            List<int> y1 = new List<int>() { 25, 35, 30, 25, 35, 25, 35, 30, 25, 35 };
-            List<int> y2 = new List<int>() { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 };
-
-            chart1.Series["张三"].Points.DataBindXY(x, y1);
-            chart1.Series["李四"].Points.DataBindXY(x, y2);
-
-            chart1.Series["张三"].ChartType = SeriesChartType.Spline;
-            chart1.Series["张三"].Color = Color.FromArgb(0, 0, 128);
-
-            chart1.Series["李四"].ChartType = SeriesChartType.Line;
-            chart1.Series["李四"].Color = Color.Red;
-
-            TLabel a = new TLabel()
+            TLabel title_label = new TLabel()
             {
                 Angle = -90
             };
-            a.Text = "喷射混凝土厚度(cm)";
-            a.Visible = true;
-            a.Width = 30;
-            a.Height = 200;
-            a.Location = new Point(50, 100);
-            this.Controls.Add(a);
+            title_label.Text = "喷射混凝土厚度(cm)";
+            title_label.Visible = true;
+            title_label.Width = 15;
+            title_label.Height = 200;
+            title_label.Font = new Font("黑体", 9, FontStyle.Regular);
+            title_label.Location = new Point(10, 200);
+            //cexianPanel.Controls.Add(title_label);
 
+            TLabel b = new TLabel();
+
+            b.Text = "里 程";
+            b.Visible = true;
+            b.Width = 100;
+            b.Height = 20;
+            b.Font = new Font("黑体", 9, FontStyle.Regular);
+            b.Location = new Point(450, 595);
+            cexianPanel.Controls.Add(b);
+
+            List<String> item = new List<String>() { "G测线", "E测线", "C测线", "A测线", "B测线", "D测线", "F测线" };
+            List<String> x = new List<String>() { "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " ", "K10+450", " ", " ", " ", " ", " ", " ", " ", " " };
+            List<String> null_x = new List<String>() { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+            List<String> y = new List<String>() { "25", "35", "30", "25", "35", "25", "35", "30", "25", "35", null, null, null, null, null, null, null };
+            for(var i = 0; i < item.Count; i++)
+            {
+                var isLast = i == item.Count - 1 ? true : false;
+                cexianPanel.Controls.Add(createChart(item[i],i, isLast, x, null_x, y));
+            }
+
+            
+
+            int startW = 2;
+            for(var i = 0; i < 11; i++)
+            {
+                string text = " ";
+                Font font = new Font("黑体", 8);
+                int width = 0;
+                Label label = new Label();
+                if (i == 0)
+                {
+                    text = "贵州省交通建设工程检测中心有限责任公司";
+                    width = 270;
+                }
+                if (i == 1)
+                {
+                    text = "保泸高速土建S1合同";
+                    width = 120;
+                }
+                if (i == 2)
+                {
+                    text = "老营特长隧道右幅进口喷射混凝土厚度图";
+                    width = 241;
+                }
+                if (i == 3)
+                {
+                    text = "制图";
+                    width = 60;
+                }
+                if (i == 3)
+                {
+                    text = "王宝强";
+                    width = 70;
+                }
+                if (i == 4)
+                {
+                    text = "复核";
+                    width = 50;
+                }
+                if (i == 5)
+                {
+                    text = "贾乃亮";
+                    width = 60;
+                }
+                if (i == 6)
+                {
+                    text = "图号";
+                    width = 50;
+                }
+                if (i == 7)
+                {
+                    text = "附图1";
+                    width = 60;
+                }
+                if (i == 8)
+                {
+                    text = "日期";
+                    width = 50;
+                }
+                if (i == 9)
+                {
+                    text = DateTime.Now.ToString("yyyy.MM.dd");
+                    width = 80;
+                }
+                label.Location = new Point(startW, 620);
+                startW += width;
+                label.Text = text;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                label.Font = font;
+                label.Paint += new PaintEventHandler(panel1_Paint2);
+                label.Width = width;
+                label.Height = 38;
+                tableP.Controls.Add(label);
+            }
+            tableP.Controls.Add(title_label);
+            tableP.Controls.Add(cexianPanel);
+            this.Controls.Add(tableP);
+            // 绘图
+            Bitmap newBitmap = new Bitmap(tableP.Width, tableP.Height);
+            tableP.DrawToBitmap(newBitmap, tableP.ClientRectangle);
+            Graphics g = Graphics.FromImage(newBitmap);
+            g.DrawImage(newBitmap, newBitmap.Width, newBitmap.Height);
+            g.Dispose();
+            newBitmap.Save(Path.Combine(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "111.png"), System.Drawing.Imaging.ImageFormat.Png);
+            newBitmap.Dispose();
+        }
+
+        private Chart createChart(String item, int index, bool isLast, List<string> x, List<string> null_x, List<string> y)
+        {
+            // 创建图表
+            Chart A = new Chart();
+            A.Width = 1085;
+            A.Height = isLast ? 110 : 80;
+            A.Location = new Point(-30, 80 * index);
+            ChartArea area = new ChartArea("ChartArea");
+            A.ChartAreas.Add(area);
+            //A.ChartAreas[0].Position = new ElementPosition(0, 0, area.Position.Width, area.Position.Height);
+            A.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
+            A.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.White;
+            A.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.FromArgb(191,  191, 191);
+
+            A.Series.Add(item);
+
+            //A.Legends.Add("E测线");
+            //A.Legends[0].ForeColor = Color.FromArgb(191, 191, 191);
+            //A.Legends[0].Position = new ElementPosition(50, 10, 7, 10);
+            //A.Legends[0].TitleAlignment = StringAlignment.Center;
+            //A.Legends[0].BackColor = Color.White;
+            //A.Legends[0].LegendStyle = LegendStyle.Row;
+            //A.Legends[0].Title = "E测线";
+            //A.Legends[0].TitleFont = new Font("黑体", 9, FontStyle.Regular);
+            // 不显示x轴
+
+            A.ChartAreas[0].AxisX.IsMarginVisible = false;
+            A.ChartAreas[0].AxisX.Enabled = AxisEnabled.True;
+            A.ChartAreas[0].AxisX.LabelStyle.Angle = 60;
+            A.ChartAreas[0].AxisX.LabelStyle.Font = new Font("黑体", 7, FontStyle.Regular);
+            A.ChartAreas[0].AxisX.LabelAutoFitMinFontSize = 6;
+            A.ChartAreas[0].AxisX.IsLabelAutoFit = false;
+            A.ChartAreas[0].AxisX.IntervalOffset = 0;
+            //A.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+                A.ChartAreas[0].AxisX.Interval = 1;
+            if (isLast)
+            {
+                A.ChartAreas[0].AxisX.MajorTickMark.Enabled = true;
+                A.ChartAreas[0].AxisX.MajorTickMark.Size = 3;
+                A.ChartAreas[0].AxisX.LineWidth = 1;
+                A.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.FromArgb(51, 51, 51);
+            } else
+            {
+                A.ChartAreas[0].AxisX.LineColor = Color.FromArgb(191, 191, 191);
+                A.ChartAreas[0].AxisX.MajorTickMark.Size = 0;
+            }
+
+
+            A.ChartAreas[0].AxisY.LineWidth = 1;
+            A.ChartAreas[0].AxisY.Maximum = 70;
+            A.ChartAreas[0].AxisY.Minimum = 10;
+            A.ChartAreas[0].AxisY.Interval = 10;
+            A.ChartAreas[0].AxisY.MajorTickMark.Size = 0.5f;
+            A.ChartAreas[0].AxisY.IsLabelAutoFit = true;
+            A.ChartAreas[0].AxisY.LabelStyle.Font = new Font("黑体", 7);
+
+            A.Series[item].Points.DataBindXY(isLast ? x : null_x, y);
+
+            A.Series[item].ChartType = SeriesChartType.Spline;
+            A.Series[item].Color = Color.FromArgb(0, 0, 128);
+
+            Label title = new Label();
+            title.AutoSize = false;
+            title.Font = new Font("黑体", 8, FontStyle.Regular);
+            title.BackColor = Color.White;
+            title.ForeColor = Color.Black;
+            title.Width = 50;
+            title.Height = 10;
+            title.Text = item;
+            title.TextAlign = ContentAlignment.MiddleCenter;
+            title.Location = new Point(500, 10);
+            A.Controls.Add(title);
+
+            Panel panel = new Panel();
+            panel.Width = 540;
+            panel.Height = 1;
+            panel.Paint += new PaintEventHandler(panel1_Paint);
+            panel.Location = new Point(72, 42);
+            A.Controls.Add(panel);
+
+            return A;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Panel panel = sender as Panel;
+            ControlPaint.DrawBorder(e.Graphics, panel.ClientRectangle,
+            Color.Red, 0, ButtonBorderStyle.Dashed, //左边
+                 Color.Red, 0, ButtonBorderStyle.Dashed, //上边
+                 Color.Red, 0, ButtonBorderStyle.Dashed, //右边
+                 Color.Red, 1, ButtonBorderStyle.Dashed);//底边
+        }
+
+        private void panel1_Paint1(object sender, PaintEventArgs e)
+        {
+            Panel panel = sender as Panel;
+            ControlPaint.DrawBorder(e.Graphics, panel.ClientRectangle,
+                Color.Black, 2, ButtonBorderStyle.Solid, //左边
+                Color.Black, 2, ButtonBorderStyle.Solid, //上边
+                Color.Black, 2, ButtonBorderStyle.Solid, //右边
+                Color.Black, 2, ButtonBorderStyle.Solid);//底边
+        }
+
+        private void panel1_Paint2(object sender, PaintEventArgs e)
+        {
+            Label panel = sender as Label;
+            ControlPaint.DrawBorder(e.Graphics, panel.ClientRectangle,
+                Color.Black, 0, ButtonBorderStyle.Solid, //左边
+                Color.Gray, 1, ButtonBorderStyle.Solid, //上边
+                Color.Black, 1, ButtonBorderStyle.Solid, //右边
+                Color.Black, 0, ButtonBorderStyle.Solid);//底边
         }
 
         private void button1_Click(object sender, EventArgs e)
